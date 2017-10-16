0af9774507dccb28dfb451a94fc76702a8a14b04
Andrew Bonner <andrewbonner92@gmail.com>
2017-10-16 13:36:55 -0400

    Add express static linking to public folder.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (request, response) => {
   response.render('home.hbs', {
